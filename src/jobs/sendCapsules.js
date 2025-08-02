b7a4deb5277b7efb82ca43db1a5a8a82fcff8ea4
vinicimiranda <vinicios.miiranda5@gmail.com>
2025-08-02 14:50:48 -0300

    Correções + personalização

--- src/jobs/sendCapsules.js
+++ src/jobs/sendCapsules.js
@@ -1,6 +1,14 @@
 import cron from 'node-cron';
+import fs from 'fs';
+import path from 'path';
+import Handlebars from 'handlebars';
 import prisma from '../prisma/client.js';
 import { sendMail } from '../utils/mailer.js';
+
+// Carrega e compila o template HTML
+const templatePath = path.resolve('src/template/capsuleTemplate.html');
+const templateSource = fs.readFileSync(templatePath, 'utf-8');
+const compileTemplate = Handlebars.compile(templateSource);
 
 cron.schedule('* * * * *', async () => {
   const now = new Date();
@@ -9,15 +17,25 @@ cron.schedule('* * * * *', async () => {
   });
 
   for (const capsule of capsules) {
+    const html = compileTemplate({
+      name: capsule.name,
+      message: capsule.message,
+      imageUrl: capsule.image || null, 
+    });
+
     await sendMail(
       capsule.email,
       'Sua Cápsula do Tempo está aqui!',
-      `<h1>${capsule.name}</h1><p>${capsule.message}</p>`
+      html
     );
 
-    await prisma.capsule.update({
-      where: { id: capsule.id },
-      data: { sent: true },
+    // await prisma.capsule.update({
+    //   where: { id: capsule.id },
+    //   data: { sent: true },
+    // });
+
+    await prisma.capsule.delete({
+      where: { id: capsule.id }
     });
   }
 });
